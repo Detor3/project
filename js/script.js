81d9d7b3f27f278673bc03ab55507b4fa1d49fad
Pavel <pfalkov9000@gmail.com>
2021-01-08 18:06:13 +0500

    change js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let humansPersonal = {
+    count: [1, 2, 3],
+    movies: {},
+    actors: [],
+    genres: [],
+    privat: false
+};
+
+function showDB(hidden) {
+    if (!hidden) {
+        console.log(humansPersonal);
+    }
+}
+
+showDB(humansPersonal.privat);
