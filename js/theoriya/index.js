0f7a7959972aaeda409cf0181a8ede52a1d27006
Pavel <pfalkov9000@gmail.com>
2021-01-08 13:07:40 +0500

    add new js file

---
+++ js/theoriya/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const str = "teSt";
+
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str); 
+
+const fruit = "Hi everybody i eat apples and pears!";
+
+console.log(fruit.indexOf(","));
+
+let logg = "Lorem ipsum dolor, sit amet consectetur!";
+console.log(logg.slice(0, 46));
+console.log(logg.substring(19, 30));
+console.log(logg.substr(0, 13));
+
+
+
+const num = 12.4999999;
+
+console.log(Math.round(num));
+
+
+const test = "15.6px"
+console.log(parseInt(test));
+console.log(parseFloat(test));
