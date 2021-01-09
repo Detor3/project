94e6d260881c25efc3aa3ef20f3e76e5025371ed
Pavel <pfalkov9000@gmail.com>
2021-01-09 18:51:04 +0500

    js change

---
+++ js/theoriya/index2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let options = {
+    name: "test", 
+    height: 1980,
+    width: 1080,
+    color: {
+        border: 'black',
+        bgc: 'grey'
+    },
+    makeTest: function() {
+        console.log("test");
+    }
+};
+options.makeTest();
+var { border, bgc } = options.color;
+console.log(border,bgc);
+// console.log(Object.keys(options).length);
+// for (const key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (const i in options[key]) {
+//             console.log(`Элемент ${i} имеет значение: ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Элемент ${key} имеет значение: ${options[key]}`);
+        
+//     }
+
+// }
+// console.log(counter);
+
+
+
+
+
