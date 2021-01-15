a39cc2a18dad235910094e130fe109149146eaca
Pavel <pfalkov9000@gmail.com>
2021-01-15 14:26:00 +0500

    new js file

---
+++ js/theoriya/index5.js
@@ -0,0 +1,23 @@
+"use strict";
+// let x = 5; alert( x++ ); // 5
+
+// console.log([] + false - null + true);// NaN
+
+// let y = 1; let x = y = 2; alert(x); // 2
+// console.log([] + 1 + 2);//12
+
+// alert("1"[0]);// 1
+
+// console.log(2 && 1 && null && 0 && undefined);//null
+// &&- всегда запинается на лжи!
+// console.log(!!( 1 && 2 ) === (1 && 2));//false
+
+// alert( null || 2 && 3 || 4 ); //3
+
+// let a = [1, 2, 3]; 
+// let b = [1, 2, 3]; 
+// console.log(a==b);//false
+
+// alert( +"Infinity" ); //infinity
+
+// console.log("Ёжик" > "яблоко");//false
