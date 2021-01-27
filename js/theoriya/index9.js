cce86cdf2461827d62267e614720b3999c3b3f9f
Pavel <pfalkov9000@gmail.com>
2021-01-27 18:58:31 +0500

    js

---
+++ js/theoriya/index9.js
@@ -0,0 +1,14 @@
+
+
+const now = new Date();
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getUTCHours());
+console.log(now.get);
+
+
+console.log(now.getTimezoneOffset());
+console.log(now.getTime());
